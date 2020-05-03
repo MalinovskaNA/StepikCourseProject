1103df09f213232e669e28ea6a2affbc1104a2a2
MalinovskayaNA <MalinovskayaNA@cbr.ru>
2020-05-03 10:49:23 +0300

    Наследование:исключение методов+заглушка

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,6 +1,5 @@
 from .base_page import BasePage
 from .locators import MainPageLocators
-from .locators import ProductPageLocators
 from selenium.webdriver.common.by import By
 from .login_page import LoginPage
 
