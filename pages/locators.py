ac109cd9a2341df248b8795649c9c3bc40a206f8
MalinovskayaNA <MalinovskayaNA@cbr.ru>
2020-05-06 12:52:31 +0300

    Дополнение тестами по заданию-наследование и отрицательные проверки

--- pages/locators.py
+++ pages/locators.py
@@ -10,7 +10,7 @@ class LoginPageLocators():
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
 
 class ProductPageLocators():
-    BUTTON_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form")  
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, "#add_to_basket_form")  
     PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main >.price_color")
     PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main >h1")
 
@@ -22,4 +22,10 @@ class ProductPageLocators():
 class BasePageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BUTTON_BASKET=(By.CSS_SELECTOR,".btn-group a.btn.btn-default")
+
+class BasketPageLocators():
+    EMPTY_BASKET_MESSAGE = (By.CSS_SELECTOR, "#content_inner p")
+    PRODUCT = (By.CSS_SELECTOR, "#content_inner h2.col-sm-6")
+
 
--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,7 @@
 import pytest
 from .pages.main_page import MainPage
 from .pages.product_page import ProductPage 
+from .pages.basket_page import BasketPage 
 import time   
 
 #@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
@@ -63,4 +64,14 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.add_to_basket()
     page.should_be_login_link()
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_not_be_product_in_basket()
+    basket_page.should_be_message_about_no_product_in_basket()
+
  
